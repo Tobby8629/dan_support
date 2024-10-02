316a3af20e96f26bececb32231960855e1836653
tobby8629 <sarhmullah15@gmail.com>
2024-10-02 10:30:38 -0500

    Project Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { HomeComponent } from './Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: "", component: HomeComponent},
+   {path: "about", component: AboutComponent},
+   {path: "service", component: ServiceComponent}
+];
